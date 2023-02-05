3c40148fb5708e430511f38edb611da5d3fe2f10
Dmitry Soldatov <soldatov_dmitry@mail.ru>
2023-02-05 16:41:32 +0700

    Измменио  ViewController

--- Xylophone marathon/ViewController.swift
+++ Xylophone marathon/ViewController.swift
@@ -21,9 +21,13 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         
         playSound(soundName: sender.currentTitle!)
+//        lorm
+       
         
         //Reduces the sender's (the button that got pressed) opacity to half.
         sender.alpha = 0.5
+            
+        
         
         //Code should execute after 0.2 second delay.
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
